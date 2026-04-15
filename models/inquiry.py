aaf4243f93d2187e9a1103744d4059576859432a
Tadiwa Gutu <u25629604@tuks.co.za>
2026-04-15 12:42:12 +0200

    Updated the setters and added grade instance attribute and class attributes as well.

--- models/inquiry.py
+++ models/inquiry.py
@@ -2,10 +2,16 @@ import heapq
 import json
 import uuid
 
+
 class Inquiry:
-    def __init__(self, name, subject, descri, urgency, submitted_at, status, claimed_by=None, id=None):
+    # for O(1) searching -> faster algorithm
+    subjects = {'Mathematics', 'Physical Sciences', 'Geography', 'Life Sciences', 'Social Sciences', 'Natural Sciences'}
+    grades = {7, 8, 9, 10, 11, 12}
+
+    def __init__(self, name, grade, subject, descri, urgency, submitted_at, status, claimed_by=None, id=None):
         self.__inquiry_id = str(uuid.uuid4()) if id is not None else None
         self.__learner_name = name
+        self.__grade = grade
         self.__subject = subject
         self.__description = descri
         self.__urgency = urgency
@@ -24,13 +30,42 @@ class Inquiry:
     def learner_name(self):
         return self.__learner_name
     
+    @learner_name.setter
+    def learner_name(self, value):
+        if not isinstance(value, str) and value == '':
+            raise ValueError("Invalid name given!")
+        self.__learner_name = value.title()
+
+    @property
+    def grade(self):
+        return self.__grade
+    
+    @grade.setter
+    def grade(self, value):
+        if not isinstance(value, (int, str, float)) and int(value) not in Inquiry.grades:
+            raise ValueError("Invalid grade given!")
+        self.__grade = int(value)
+    
     @property
     def subject(self):
         return self.__subject
     
+    @subject.setter
+    def subject(self, value):
+        #Have subject set or dictionary for subjects you offer and verify with grade too
+        if not isinstance(value, str) and value.title() not in Inquiry.subjects:
+            raise ValueError("Invalid subject given!")
+        self.__subject = value.title()
+    
     @property
     def description(self):
         return self.__description
+    
+    @description.setter
+    def description(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Invalid description given!")
+        self.__description = value.capitalize()
     
     @property
     def urgency(self):
@@ -47,4 +82,8 @@ class Inquiry:
     @property
     def claimed_by(self):
         return self.__claimed_by
+    
+    
+
+        
     
