d40f4d66e7a7849a5de2ee4a8f02fbbc5d0c4f0d
Tadiwa Gutu <u25629604@tuks.co.za>
2026-04-17 10:14:34 +0200

    Began implementing TutoringQueue

--- queues/tutoring_queue.py
+++ queues/tutoring_queue.py
@@ -1,4 +1,10 @@
 import heapq
+import models
 
 class TutoringQueue:
-    pass
+    def __init__(self):
+        self.__heap = list() # empty queue
+        self.__history = list() # list of past inquiries
+        self.__counter = None # an integer value comparing inquiries that have the same urgency but different times
+    
+    # implement the methods 
