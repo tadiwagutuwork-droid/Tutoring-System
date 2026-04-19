7dbb25cb1fe95932d5eba0e3b85da88b66b37f55
Tadiwa Gutu <u25629604@tuks.co.za>
2026-04-19 17:09:25 +0200

    Testing heap

--- testing.py
+++ testing.py
@@ -8,7 +8,8 @@ u = m.UrgencyLevel.return_urgency(1)
 instance = m.Inquiry('Tadiwa', 8, 'Mathematics', 'Functions: Exponential', u, day)
 instance.claimed_by = 'Justus'
 instance.grade = 10
-print(instance)
-print(instance.__repr__)
 
 queue = q.TutoringQueue()
+queue.enqueue(instance)
+
+print(queue.heap)
